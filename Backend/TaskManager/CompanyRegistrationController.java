59cae865b4c5a1a10c7ac87aefbfbe417714ee16
Akshar Patel <Akshar Patel>
2018-04-25 08:17:21 -0700

    Added controller to access form data and register user

---
+++ Backend/TaskManager/CompanyRegistrationController.java
@@ -0,0 +1,31 @@
+package TaskManager;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/companyregistration")
+public class CompanyRegistrationController {
+	@RequestMapping("/showSignUp")
+	public String showSignUp() {
+		return "signUp";
+	}
+	@RequestMapping("/fetchAdminandValidateData")
+	public String fetchAdminandValidateData(@RequestParam("firstname") String firstname, 
+			@RequestParam("lastname") String lastname
+			,@RequestParam("title") String title,
+			@RequestParam("username") String username,
+			@RequestParam("department") String department,
+			@RequestParam("workphone") String workphone,
+			@RequestParam("email") String email,
+			@RequestParam("location") String location,
+			@RequestParam("password") String password, Model model){
+		String[] data = {firstname + " " +lastname, workphone, email, location, username, password, title, department};
+		Registration register = new UserRegistration("admin", data); 
+		register.print();
+		return "signUp";
+	}
+}
