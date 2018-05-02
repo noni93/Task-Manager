24d6dd9843903c1831156b496228ccbbd523f39a
Akshar Patel <Akshar Patel>
2018-05-01 21:04:41 -0700

    Added User registration class that registers user to the database

--- TaskManager/src/TaskManager/UserRegistration.java
+++ TaskManager/src/TaskManager/UserRegistration.java
@@ -9,24 +9,46 @@ public class UserRegistration implements Registration {
 	
 	private User user;
 	private String[] passwordToValidate;
+
 	public UserRegistration(String which_user, String data[]) {
 		init(which_user, data);
 		passwordToValidate = new  String[] {data[5], data[8]};
+	
+	}
+	
+	private Object[] validateData() {
+		// TODO Auto-generated method stub
+		validateUserRegistration v = new validateUserRegistration(user, passwordToValidate[0], passwordToValidate[1]);
+		
+		return new Object[] {v.validationResult(), v};
 	}
 	@Override
-	public boolean validateData() {
+	public String register(String... args) {
 		// TODO Auto-generated method stub
-		return false;
+		Object[] results = validateData();
+		boolean result = (Boolean)results[0];
+		validateUserRegistration v = (validateUserRegistration) results[1];
+		if(!result) {
+			String errors ="";
+			for(String error: v.errors) {
+				errors += error + "\n";
+			}
+			v.clean();
+			return errors;
+		}else {
+			// Write code to add User user to the tableName field of this class
+			String table_name = args[0];
+			v.clean();
+			return "";
+		}
+		
 	}
-	@Override
-	public boolean register() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+
 	@Override
 	public boolean clean() {
-		// TODO Auto-generated method stub
-		return false;
+		user = null;
+		passwordToValidate= null;
+		return (user == null && passwordToValidate == null);
 	}
 	public void print() {
 		user.print();
